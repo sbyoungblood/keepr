b733a1a2949040a7967cc9cdb9c7cc34ea7a6e57
Scotty Youngblood <scottybyoungblood@gmail.com>
2023-04-03 11:58:08 -0600

    front end masonry

--- keepr.client/src/pages/HomePage.vue
+++ keepr.client/src/pages/HomePage.vue
@@ -1,44 +1,72 @@
 <template>
-  <div class="home flex-grow-1 d-flex flex-column align-items-center justify-content-center">
-    <div class="home-card p-5 bg-white rounded elevation-3">
-      <img
-        src="https://bcw.blob.core.windows.net/public/img/8600856373152463"
-        alt="CodeWorks Logo"
-        class="rounded-circle"
-      >
-      <h1 class="my-5 bg-dark text-white p-3 rounded text-center">
-        Vue 3 Starter
-      </h1>
-    </div>
+  <div class="container-fluid">
+    <section class="bricks">
+      <div v-for="k in keeps">
+        <KeepCard :keep="k" />
+      </div>
+    </section>
   </div>
 </template>
 
 <script>
+import { logger } from "../utils/Logger";
+import Pop from "../utils/Pop";
+import { keepsService } from "../services/KeepsService.js"
+import { computed, onMounted } from "vue";
+import { AppState } from '../AppState.js';
+
 export default {
   setup() {
-    return {}
+
+    onMounted(() => {
+      getKeeps()
+    })
+
+    async function getKeeps() {
+      try {
+        await keepsService.getKeeps()
+      } catch (error) {
+        logger.error(error)
+        Pop.error(error)
+      }
+    }
+    return {
+      keeps: computed(() => AppState.keeps)
+    }
   }
 }
 </script>
 
 <style scoped lang="scss">
-.home {
-  display: grid;
-  height: 80vh;
-  place-content: center;
-  text-align: center;
-  user-select: none;
+// .home {
+//   display: grid;
+//   height: 80vh;
+//   place-content: center;
+//   text-align: center;
+//   user-select: none;
 
-  .home-card {
-    width: 50vw;
+//   .home-card {
+//     width: 50vw;
 
-    >img {
-      height: 200px;
-      max-width: 200px;
-      width: 100%;
-      object-fit: contain;
-      object-position: center;
-    }
+//     >img {
+//       height: 200px;
+//       max-width: 200px;
+//       width: 100%;
+//       object-fit: contain;
+//       object-position: center;
+//     }
+//   }
+// }
+
+$gap: .5em;
+
+.bricks {
+  columns: 250px;
+  column-gap: $gap;
+
+  &>div {
+    margin-top: $gap;
+    display: inline-block;
   }
 }
 </style>
